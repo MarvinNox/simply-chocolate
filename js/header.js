8cb099bfc18ac4956fb3364c3c70173c4c73b4a8
Marvin Nox <sinedtex@gmail.com>
2024-12-13 00:04:58 +0200

    add some animation

--- js/header.js
+++ js/header.js
@@ -2,10 +2,10 @@ const header = document.getElementById('header-scroll');
 
 window.addEventListener('scroll', () => {
     if (window.scrollY > 50) {
-        header.style.background = 'linear-gradient(to right, #659999, #f4791f)';
+        header.style.backgroundImage = 'linear-gradient(to right, #659999, #f4791f)';
         // header.style.color = '#fff'
     } else {
-        header.style.background = 'none'
+        header.style.backgroundImage = 'none'
         header.style.backgroundColor = '#fff'
         // header.style.color = '#000'
     }
