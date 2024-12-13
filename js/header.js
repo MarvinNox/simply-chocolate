80cf5cf0a1eb36ac2b6315766c7e30169de4d3e5
Marvin Nox <sinedtex@gmail.com>
2024-12-13 16:33:29 +0200

    header css anim, fxs hover

--- js/header.js
+++ js/header.js
@@ -3,10 +3,12 @@ const header = document.getElementById('header-scroll');
 window.addEventListener('scroll', () => {
     if (window.scrollY > 50) {
         header.style.backgroundImage = 'linear-gradient(to right, #659999, #f4791f)';
+           header.style.opacity = '0.9';
         // header.style.color = '#fff'
     } else {
         header.style.backgroundImage = 'none'
         header.style.backgroundColor = '#fff'
+                   header.style.opacity = '1';
         // header.style.color = '#000'
     }
 });
