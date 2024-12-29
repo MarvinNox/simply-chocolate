6506f49787b89e26f51123dc97913195537faa24
Marvin Nox <sinedtex@gmail.com>
2024-12-29 18:56:48 +0200

    anim fx

--- js/modal.js
+++ js/modal.js
@@ -1,3 +1,4 @@
+'use strict'
 const modalOpener = document.getElementById("modal-window");
 const openBttn = document.getElementById("open-modal");
 const closeBttn = document.getElementById("close-modal");
@@ -9,6 +10,7 @@ openBttn.addEventListener("click", () => {
 closeBttn.addEventListener("click", () => {
   modalOpener.classList.remove("is-open");
 });
+// Функція закриття модального вікна при кліку поза межами вікна
 window.addEventListener("click", (event) => {
   if (event.target === modalOpener) {
   modalOpener.classList.remove("is-open");
